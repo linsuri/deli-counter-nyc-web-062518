81e2288e6bea379ad7f4f31b2dd598c73b054299
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-18 14:20:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,4 +7,4 @@ def line(katz_deli)
     "The line is currently empty."
   else
     output = "The line is currently: "
-    katz_deli.each_with_index {|name, index| output += #{
+    katz_deli.each_with_index {|name, index| output += "#{index+1}. " +
