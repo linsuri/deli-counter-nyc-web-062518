d92d5cd851491657a4939ea6219b959757a5c033
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-18 14:42:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(katz_deli, new_person)
   katz_deli.push(new_person)
-  puts "Welcome, #{new_person}. You are number #{katz_deli.index(new_person) + 1} in line."}
+  puts "Welcome, #{new_person}. You are number #{katz_deli.index(new_person)} in line."}
 end
 
 def now_serving(katz_deli)
